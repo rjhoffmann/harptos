80ee6e26c17ef7b633a4e47373603b67dc80f3aa
Ryan Hoffmann <ryan.hoffmann@airwis.com>
2018-06-04 17:50:51 -0500

    task: styling

--- src/App/containers/calendar/index.js
+++ src/App/containers/calendar/index.js
@@ -1,59 +1,38 @@
 import React from 'react';
 import _find from 'lodash/find';
 
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+
 import calendar from '../../data/calendar';
 import years from '../../data/years';
 
-import Selune from '../../components/selune';
+class Calendar extends React.Component {
+  constructor(props) {
+    super(props);
 
-import { Calendar } from '../../styles/calendar';
-import { MonthContainer, Month, MonthHeader } from '../../styles/calendar/month';
-import { Week } from '../../styles/calendar/week';
-import { Day, DayContainer } from '../../styles/calendar/day';
+    this.state = {
+      year: _find(years, { year: 1493 }),
+      months: calendar.months,
+    };
+  }
 
-export default ({ match }) => {
-  const { year, name: yearName } = _find(years, {
-    // Defaulting to 1493 for ... reasons.
-    year: parseInt((match.params.year || 1493), 10)
-  });
+  render() {
+    const { year, name: yearName } = this.state.year;
 
-  console.log(calendar);
-
-  // Every 4 years is a leap year for this calendar.
-  const isLeapYear = year%4 === 0;
-
-  return (
-    <Calendar>
-      {`${year}DR - ${yearName}`}
-      {calendar.months.map(month => (
-        <MonthContainer key={month.id}>
-          <Month>
-            <MonthHeader>{month.name}</MonthHeader>
-            {month.weeks.map(week => (
-              <Week key={week.id}>
-                {week.days.map(day => (
-                  <DayContainer key={day.id}>
-                    <Day>
-                      {day.id}
-                      <Selune year={year} month={month.id} day={day.id} />
-                    </Day>
-                  </DayContainer>
-                ))}
-              </Week>
-            ))}
-          </Month>
-          {month.holiday && <div>
-            <span>{month.holiday.name}</span>
-            <Selune year={year} month={month.id} day={31} />
-          </div>}
-          {isLeapYear && month.id === 7 &&
-            <div>
-              <span>Shieldmeet</span>
-              <Selune year={year} month={month.id} day={32} />
-            </div>
-          }
-        </MonthContainer>
-      ))}
-    </Calendar>
-  )
+    return (
+      <div>
+        <AppBar position="static">
+          <Toolbar>
+            <Typography variant="title" color="inherit">
+              {`${year}DR - ${yearName}`}
+            </Typography>
+          </Toolbar>
+        </AppBar>
+      </div>
+    )
+  }
 };
+
+export default Calendar;
---
+++ src/App/styles/theme.js
@@ -0,0 +1,14 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+import grey from '@material-ui/core/colors/grey';
+import blueGrey from '@material-ui/core/colors/blueGrey';
+
+export const rally = createMuiTheme({
+  palette: {
+    primary: {
+      main: blueGrey['800']
+    },
+    secondary: {
+      main: grey['800']
+    }
+  }
+})
