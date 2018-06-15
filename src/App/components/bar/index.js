ef214c24880c09e77bc4905dc9f9a457750317d0
Ryan Hoffmann <ryan@hoff.pub>
2018-06-15 15:15:50 -0500

    task: year switching

--- src/App/components/bar/index.js
+++ src/App/components/bar/index.js
@@ -3,13 +3,32 @@ import React from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton'
+import ArrowLeft from '@material-ui/icons/ArrowBack';
+import ArrowRight from '@material-ui/icons/ArrowForward';
+import { withStyles } from '@material-ui/core/styles';
 
-export default ({ year, month }) => (
-  <AppBar>
-    <Toolbar>
+const styles = {
+  toolbar: {
+    display: 'flex',
+    justifyContent: 'space-between'
+  }
+}
+
+const Bar = ({ year, month, classes }) => (
+  <AppBar position="static" color="default">
+    <Toolbar className={classes.toolbar}>
+      <IconButton>
+        <ArrowLeft />
+      </IconButton>
       <Typography variant="title" color="inherit">
         {`${year.year}DR - ${year.name}`}
       </Typography>
+      <IconButton>
+        <ArrowRight />
+      </IconButton>
     </Toolbar>
   </AppBar>
 );
+
+export default withStyles(styles)(Bar);
